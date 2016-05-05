1d7e191b86e3102dd1ac862e71cc6c7895e79540
swescott17 <swescott17@my.whitworth.edu>
2016-05-05 14:11:51 -0700

    Update Player.h

--- Final Project/Final Project/Player.h
+++ Final Project/Final Project/Player.h
@@ -1,3 +1,6 @@
+#ifndef _H_PLAYER_
+#define _H_PLAYER_
+
 #include<iostream>
 #include<vector>
 #include<string>
@@ -8,7 +11,7 @@ class Player
 private:
 	int numberOfCards; //number of cards that a player has
 	string name; //name of player
-	Card* hand; //pointer to array of cards currently held by player
+	vector<card> hand; //pointer to array of cards currently held by player
 public:
 	Player(string name); //constructor
 	card endTurn(bool attack); //draw from pile, draw 2 if attacked
@@ -18,8 +21,10 @@ public:
 	void nope(); //negates any other card
 	void attack(); //force another player to take 2 turns
 	void skip(); //don't take a turn at all
-	card favor(player p); //force another player to give you a card
+	card favor(Player p); //force another player to give you a card
 	void shuffle(); //mix up the cards in the draw pile
-	void seeFuture(vector pile); //look 3 cards into the draw pile
-	card pair(player* p); //take a random card from anoter player
+	void seeFuture(vector <card> pile); //look 3 cards into the draw pile
+	card pair(Player* p); //take a random card from anoter player
 };
+
+#endif
