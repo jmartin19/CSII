13f4a62f83220e8e71ae38659682260e06c0f550
swescott17 <swescott17@my.whitworth.edu>
2016-05-15 15:48:37 -0700

    Add files via upload
    Includes getDead and the "dead if" statement

--- Final Project/Final Project/player.cpp
+++ Final Project/Final Project/player.cpp
@@ -1,28 +1,73 @@
-#include "player.h"
-#include "card.h"
-#include <time.h>
-#include <vector>
-#include <string>
-#include <iostream>
+#include"Player.h"
+#include"card.h"
 
-using namespace std;
-
+Player::Player() //default constructor for the player class
+{
+	name = "playername"; //intialize hands, intialize number of cards to 5 (1 defuse and 4 ordinary cards)
+	numberOfCards = 5;
+	hand.push_back(card("Defuse"));
+	dead = false;
+}
 
 Player::Player(string playername, vector<card> pile) //constructor for the player class
 {
 	name = playername; //intialize hands, intialize number of cards to 5 (1 defuse and 4 ordinary cards)
 	numberOfCards = 5;
-	vector <card> hand;
-	card cardef("defuse");
-	hand[0] = cardef;
-	for (int i = 1; 1 < numberOfCards; i++)
+	hand.push_back(card("Defuse"));
+	for (int i = 1; i < numberOfCards; i++)
 	{
-		hand[i] = pile[0]; //draws 4 more cards into player's hand
-		pile.erase(pile.begin()); //erases the cards that were drawn from the pile
+		hand.push_back(pile.back()); //draws 4 more cards into player's hand
+		pile.pop_back(); //erases the cards that were drawn from the pile
+	}
+	dead = false;
+}
+
+card Player::getCard(int x) //gets a card from a player's hand, delete from player's hand
+{
+	card returnCard = hand[x]; //card to be returned
+	hand.erase(hand.begin() + x); //delete played card from player's hand
+	return returnCard;
+}
+
+void Player::endTurn(bool & attack, bool & skip, vector<card> & pile)
+{
+
+	int turns; //# of times player must draw, determined by attack
+	if (attack == true) //declares integer used to force player to take correct number of turns
+		turns = 2;
+	else if (skip == true) //declares integer used to force player to take correct number of turns
+		turns = 0;
+	else
+		turns = 1;
+
+	while (turns != 0)
+	{
+		numberOfCards++; //increment total number of cards held by player
+		hand.push_back(pile.back()); //ADD TOP CARD FROM DECK
+		pile.pop_back(); //delete card from top of deck
+		turns--; //ends loop if player has taken appropriate number of turns
+	}
+
+}
+
+
+void Player::attack(bool & attack) //force the next player to take 2 cards instead of 1
+{
+	attack = true;
+}
+
+void Player::shuffle(vector<card> pile) //shuffles the vector called pile (the deck)
+{
+	int numbercard = pile.size(); //gets the size of the pile at this point
+	srand(time(NULL));  //starts a random number generator based on time
+	for (int i = 0; i < numbercard; i++) //shuffles the cards by using the vector function swap
+	{
+		int n = i + rand() % (numbercard - i);
+		swap(pile[i], pile[n]);
 	}
 }
 
-string Player::lookUp(int num) //looks up a card in the players hand
+void Player::lookUp(int num) //looks up a card in the players hand
 {
 	string name = hand[num].getType(); //gets the card type to compare to
 
@@ -59,64 +104,26 @@ string Player::lookUp(int num) //looks up a card in the players hand
 	if (name == "Tacocat" || name == "Cattermelon" || name == "Hairy Potato Cat" || name == "Rainbow-Ralphing Cat" || name == "Beard Cat")
 	{
 		cout << "A funny image of a cat is on this card. If you have another cat card, you could pair them up and play them like an action card. Playing a pair allows you to steal another player's card from their hand without looking." << endl;
-			
+
 	}
 	if (name == "Defuse")
 	{
 		cout << "You can successfully defuse an exploding kitten." << endl;
-		
+
 	}
 	else
 	{
 		cout << "Error. No such card." << endl;
-	
+
 	}
 }
 
-void Player::shuffle(vector<card> pile) //shuffles the vector called pile (the deck)
+void Player::showHand() //display cards in a player's hand
 {
-	int numbercard = pile.size(); //gets the size of the pile at this point
-	srand(time(NULL));  //starts a random number generator based on time
-	for (int i = 0; i < numbercard; i++) //shuffles the cards by using the vector function swap
+	for (int i = 0; i < hand.size(); i++)
 	{
-		int n = i + rand() % (numbercard - i);
-		swap(pile[i], pile[n]);
+		cout << i << ". " << hand[i].getType() << endl;
 	}
-}
-
-void Player::seeFuture(vector <card> pile) //player looks at the top three cards of the pile (the deck)
-{
-	
-	cout << "Other players should turn away!" << endl;
-	cout << "Card on top: " << pile[0].getType() << "." << endl; //first card
-	cout << "Second card: " << pile[1].getType() << "." << endl; //second card
-	cout << "Third card: " << pile[2].getType() << "." << endl; //third card
-}
-
-card Player::endTurn(bool& attack, bool& skip)
-{
-	int turns; //# of times player must draw, determined by attack
-	if (attack == true) //declares integer used to force player to take correct number of turns
-		turns = 2;
-	else if (skip == true) //declares integer used to force player to take correct number of turns
-		turns = 0;
-	else
-		turns = 1;
-
-	while (turns != 0)
-	{
-		numberOfCards++; //increment total number of cards held by player
-		hand.push_back(card); //ADD TOP CARD FROM DECK
-							  /*DELETE TOP CARD FROM DECK*/
-		turns--; //ends loop if player has taken appropriate number of turns
-	}
-
-}
-
-
-void Player::attack(bool & attack)
-{
-	attack = true;
 }
 
 card Player::favor(Player p)
@@ -128,3 +135,44 @@ card Player::favor(Player p)
 	card returnCard = p.hand[choice]; //card to be returned
 	return returnCard;
 }
+
+void Player::seeFuture(vector <card> pile) //player looks at the top three cards of the pile (the deck)
+{
+
+	cout << "Other players should turn away!" << endl;
+	cout << "Card on top: " << pile[0].getType() << "." << endl; //first card
+	cout << "Second card: " << pile[1].getType() << "." << endl; //second card
+	cout << "Third card: " << pile[2].getType() << "." << endl; //third card
+}
+
+void Player::skip(bool & skip)
+{
+	skip = true;
+}
+
+card Player::pair(Player p)
+{
+	cout << "Enter the name of player you want to take a card from: ";
+	cin >> p.name;// prompts the current player to take a random card from from a selected player.
+	cout << endl << p.name << ", takes a card from a selected player" << endl;
+	int choice = 0; //card selected from player will be stored in this variable.
+	cin >> choice; //store player's choice.
+	card returnCard = p.hand[choice - 1];
+	return returnCard;
+}
+
+string Player::getName()
+{
+	return name;
+}
+
+bool Player::getDead()
+{
+	return dead;
+}
+
+//What I would write in the "for loop" for the player turns in main
+//if(Player[n].getDead() == true)
+// {
+//   Player[n].skip(Skip);
+// }
--- Final Project/Final Project/player.h
+++ Final Project/Final Project/player.h
@@ -4,7 +4,10 @@
 #include<iostream>
 #include<vector>
 #include<string>
-using namespace std;
+#include<ctime>
+#include "card.h"
+using std::string;
+using std::vector;
 
 class Player
 {
@@ -12,18 +15,22 @@ private:
 	int numberOfCards; //number of cards that a player has
 	string name; //name of player
 	vector<card> hand; //pointer to array of cards currently held by player
+	bool dead;
 public:
+	Player(); //default constructor
 	Player(string name, vector<card> pile); //constructor
-	card endTurn(bool& attack, bool& skip); //draw from pile, draw 2 if attacked
+	card getCard(int x); //get a card from a player's hand, delete from player's hand
+	void endTurn(bool & attack, bool & skip, vector<card> & pile); //draw from pile, draw 2 if attacked
 	void showHand(); //display cards in hand of player
-	void playCard(int num); //plays card designated as passed-in int
-	string lookUp(int num); //displays instructions for card of passed-in int
+	void lookUp(int num); //looks up a card in the players hand
 	void attack(bool & attack); //force another player to take 2 turns
-	void skip(); //don't take a turn at all
+	void skip(bool & skip); //don't take a turn at all
 	card favor(Player p); //force another player to give you a card
 	void shuffle(vector<card> pile); //mix up the cards in the draw pile
 	void seeFuture(vector<card> pile); //look 3 cards into the draw pile
-	card pair(Player* p); //take a random card from anoter player
+	card pair(Player p); //take a random card from anoter player
+	string getName();//return name of player
+	bool getDead();//returns if the player is dead or not
 };
 
 #endif
