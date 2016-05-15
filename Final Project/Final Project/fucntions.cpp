1fe5fa3210ebc1b0c24ef587534b393e4e3c2430
swescott17 <swescott17@my.whitworth.edu>
2016-05-15 14:15:57 -0700

    Functions.cpp and functions.h
    These functions are separate from the classes and only affect the deck.

---
+++ Final Project/Final Project/fucntions.cpp
@@ -0,0 +1,13 @@
+#include "functions.h"
+#include <time.h>
+
+void shuffleDeck(vector<card> pile)
+{
+	int numbercard = pile.size(); //gets the size of the pile at this point
+	srand(time(NULL));  //starts a random number generator based on time
+	for (int i = 0; i < numbercard; i++) //shuffles the cards by using the vector function swap
+	{
+		int n = i + rand() % (numbercard - i);
+		swap(pile[i], pile[n]);
+	}
+}
---
+++ Final Project/Final Project/functions.h
@@ -0,0 +1,12 @@
+#ifndef _H_FUNCTIONS_
+#define _H_FUNCTIONS_
+
+#include "card.h"
+#include <vector>
+using namespace std;
+
+void shuffleDeck(vector<card> pile); //seperate function to shuffle the deck
+
+
+
+#endif 
